13dfbe73c98b750be012e2d265770ce76bdab190
Potato[VIP] <189317152+Ombryal@users.noreply.github.com>
2026-09-13 23:09:59 +1200

    feat(lexer): register stylesheet and meta keywords
    Maps the stylesheet and meta source words to their new TokenType values.

--- src/webforge/lexer/Keywords.h
+++ src/webforge/lexer/Keywords.h
@@ -8,18 +8,20 @@
 namespace webforge {
 
 inline const std::unordered_map<std::string, TokenType> KEYWORDS = {
-    {"page",      TokenType::KeywordPage},
-    {"text",      TokenType::KeywordText},
-    {"button",    TokenType::KeywordButton},
-    {"on",        TokenType::KeywordOn},
-    {"click",     TokenType::KeywordClick},
-    {"alert",     TokenType::KeywordAlert},
-    {"heading",   TokenType::KeywordHeading},
-    {"image",     TokenType::KeywordImage},
-    {"link",      TokenType::KeywordLink},
-    {"container", TokenType::KeywordContainer},
-    {"list",      TokenType::KeywordList},
-    {"item",      TokenType::KeywordItem}
+    {"page",       TokenType::KeywordPage},
+    {"text",       TokenType::KeywordText},
+    {"button",     TokenType::KeywordButton},
+    {"on",         TokenType::KeywordOn},
+    {"click",      TokenType::KeywordClick},
+    {"alert",      TokenType::KeywordAlert},
+    {"heading",    TokenType::KeywordHeading},
+    {"image",      TokenType::KeywordImage},
+    {"link",       TokenType::KeywordLink},
+    {"container",  TokenType::KeywordContainer},
+    {"list",       TokenType::KeywordList},
+    {"item",       TokenType::KeywordItem},
+    {"stylesheet", TokenType::KeywordStylesheet},
+    {"meta",       TokenType::KeywordMeta}
 };
 
 } // namespace webforge
